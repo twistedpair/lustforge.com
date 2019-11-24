9d2d1318748523182357f8a6bc115c026ddccdcc
Joseph Lust <lifeoflust@gmail.com>
2019-11-24 14:30:02 -0500

    Setup secrets

--- deploy_site.sh
+++ deploy_site.sh
@@ -5,10 +5,7 @@ set -eu
 DISTRIBUTION_ID=E2BRJGICC6S6CS
 BUCKET_NAME=lustforge-cdn
 
-# place AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY into your GH secrets
-export AWS_ACCESS_KEY_ID=${{ secrets.AWS_ACCESS_KEY_ID }}
-export AWS_SECRET_ACCESS_KEY=${{ secrets.AWS_SECRET_ACCESS_KEY }}
-
+# place AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY into your GH repo secrets
 if [[ -z ${AWS_ACCESS_KEY_ID} ]]; then
   echo "secrets.AWS_ACCESS_KEY_ID must defined in your GitHub secrets"
   exit 1
@@ -20,8 +17,6 @@ if [[ -z ${AWS_SECRET_ACCESS_KEY} ]]; then
 fi
 
 # Invalidate landing page so everything sees new post - warning, first 1K/mo free, then 1/2 cent each
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths /index.html /
 
 # commit and push
