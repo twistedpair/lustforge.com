b4a0f5995a6d02650771c545a7faa276b80c6a76
Joseph Lust <lifeoflust@gmail.com>
2019-11-24 14:26:02 -0500

    Auto build/commit/invaldiate

--- deploy_site.sh
+++ deploy_site.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 set -eu
 
-# Run this in your Hugo blog repo directory
-
 # These are mine. They won't work for you :)
 DISTRIBUTION_ID=E2BRJGICC6S6CS
 BUCKET_NAME=lustforge-cdn
-PROFILE=lauf # or `default` if you don't use profiles
 
-hugo -v -d docs/
+# place AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY into your GH secrets
+export AWS_ACCESS_KEY_ID=${{ secrets.AWS_ACCESS_KEY_ID }}
+export AWS_SECRET_ACCESS_KEY=${{ secrets.AWS_SECRET_ACCESS_KEY }}
+
+if [[ -z ${AWS_ACCESS_KEY_ID} ]]; then
+  echo "secrets.AWS_ACCESS_KEY_ID must defined in your GitHub secrets"
+  exit 1
+fi
+
+if [[ -z ${AWS_SECRET_ACCESS_KEY} ]]; then
+  echo "secrets.AWS_SECRET_ACCESS_KEY must defined in your GitHub secrets"
+  exit 1
+fi
 
 # Invalidate landing page so everything sees new post - warning, first 1K/mo free, then 1/2 cent each
-aws cloudfront create-invalidation --profile ${PROFILE} --distribution-id ${DISTRIBUTION_ID} --paths /index.html /
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths /index.html /
 
-# TODO commit
-# TODO push
+# commit and push
+git add docs/
+git commit -m "Publish github pages"
+git push origin HEAD
